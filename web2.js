ea912426f905288b077b6e539c670bc43c1963ac
Sala White <white@academia.loc>
2018-03-12 10:40:11 -0500

    modulos

---
+++ web2.js
@@ -0,0 +1,13 @@
+import {Persona} from 'persona.js';
+import {TITULO_PAGINA} from 'constante.js';
+
+document.getElementById('titulo').innerHTML= TITULO_PAGINA;
+
+let nombre = prompt("digite su nombre");
+let edad = prompt("digite su edad");
+
+let persona= new Persona(nombre,edad);
+
+document.getElementById('nombre').innerHTML=persona.getNombre();
+document.getElementById('edad').innerHTML=persona.getEdad();
+document.getElementById('saludo').innerHTML=persona.saludo();
